4a4ae7b4fc5c0381561fc045607b81fc2b724f05
techotrialadmin <info@techtorialacademy.com>
2019-07-15 20:33:27 -0500

    First commit for Jenkins

---
+++ src/test/java/com/techtorial/jenkins/jenkinsExec.java
@@ -0,0 +1,10 @@
+package com.techtorial.jenkins;
+
+public class jenkinsExec {
+
+
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--)
+            System.out.println("I'm perfect in Jenkins");
+    }
+}
